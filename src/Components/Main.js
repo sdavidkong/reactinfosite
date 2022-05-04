3b097a362d474dc78568b8750768b97e993082e4
S David Kong <SDAVIDKONG@gmail.com>
2022-05-04 20:12:57 +0300

    finished reactinfo

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Main() {
-    return <h1>Main goes here</h1>
+    return (
+        <div className="maintext">
+    <h1>Fun facts about React</h1>
+    <ul className="facts">
+        <li>Released in 2013</li>
+        <li>Was created by Jordan Walker</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is Maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+
+    </ul>
+    </div>
+        )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Logo from "../logo.svg"
 
 export default function Navbar() {
-    return <h1>Navbar goes here</h1>
+    return (
+    <nav className="nav">
+        <img src={Logo} width="130px" alt="" className="nav--icon" />
+        <h3 className="nav--logo_text">ReactFacts</h3>
+        <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+        )
 }
